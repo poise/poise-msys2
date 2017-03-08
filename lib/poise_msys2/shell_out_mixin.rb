55ad47c8822fe84302245b9ccaf23733e8defe61
Noah Kantrowitz <noah@coderanger.net>
2017-03-07 23:01:36 -0800

    Upgrade the mixin to understand array commands using the usual Unix semantics.

--- lib/poise_msys2/shell_out_mixin.rb
+++ lib/poise_msys2/shell_out_mixin.rb
@@ -16,12 +16,19 @@
 
 require 'shellwords'
 
+require 'poise/utils/shell_out'
+
 
 module PoiseMsys2
   # Mixin for providers to run commands in the MSYS2 environment.
   #
   # @since 1.0.0
   module ShellOutMixin
+    include Poise::Utils::ShellOut
+
+    # Folders within the MSYS2 root to use for executable lookups.
+    MSYS2_PATH = %w{/usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin}
+
     def msys_shell_out(*cmd, cwd: nil, environment: {}, **options)
       # Work out the root folder.
       root = if new_resource.respond_to?(:root) && new_resource.root
@@ -39,19 +46,46 @@ module PoiseMsys2
       environment['HOME'] ||= "/home/#{ENV['USER']}"
       # Default working directory for safety.
       cwd ||= root
-      # We need to get a single string to pass to bash.
+      # Deal with whatever nesting and nulls we have.
       cmd.flatten!
-      cmd = if cmd.length == 1
-        cmd.first
+      cmd.compact!
+      real_cmd = if cmd.length == 1
+        # Running in shell wrapper mode to mimic Ruby.
+        ["#{root}/usr/bin/bash.exe", '-l', '-c', cmd.first]
       else
-       Shellwords.join(cmd)
+        # Try and do a path-ish lookup.
+        [msys_which(cmd.first, root, path: environment['PATH']) || cmd.first] + cmd.drop(1)
       end
       # Run the command via bash (for now).
-      poise_shell_out(["#{root}/usr/bin/bash.exe", '-l', '-c', cmd], cwd: cwd, environment: environment, **options)
+      poise_shell_out(real_cmd, cwd: cwd, environment: environment, **options)
     end
 
     def msys_shell_out!(*args)
       msys_shell_out(*args).tap(&:error!)
+    end
+
+    def msys_which(cmd, root, path: nil)
+      # If it was already absolute, just return that.
+      return cmd if cmd =~ /^(\/|([a-z]:)?\\)/i
+      paths = if path
+        path.split(File::PATH_SEPARATOR)
+      else
+        # Executable has a relative path, so we need to pretend to do a $PATH
+        # lookup. This is hard because we can't see the MSYS $PATH so just
+        # fake it and hope it is good enough.
+        MSYS2_PATH.map {|path_suffix| ::File.join(root, path_suffix) }
+      end
+      # Always check without an extension first just in case.
+      path_exts = [''] + (ENV['PATHEXT'] ? ENV['PATHEXT'].split(::File::PATH_SEPARATOR).map(&:strip) : [])
+      # Based on Chef::Mixin::Which#which
+      # Copyright 2010-2017, Chef Softare, Inc.
+      paths.each do |candidate_path|
+        path_exts.each do |candidate_ext|
+          filename = ::File.join(candidate_path, cmd + candidate_ext)
+          return filename.gsub(/\//, '\\') if ::File.executable?(filename)
+        end
+      end
+      false
     end
 
   end
